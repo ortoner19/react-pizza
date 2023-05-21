ee5a4b42382d8f5f0cb5a9cdd7c2b8d512eec366
ortoner19 <ortoner@ukr.net>
2023-05-21 14:56:22 +0300

    lesson15

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux'
-import { setCategoryId, setCurrentPage  } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters  } from '../redux/slices/filterSlice';
 import Categories from "../components/Categories";
-import Sort from "../components/Sort";
+import Sort, { list } from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
 // import pizzas from './assets/pizzas.json'
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
+// import sortList from '../components/Sort'
+
 
 const Home = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const isSearch = React.useRef(false);
+  const isMounted = React.useRef(false);
+
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
 
   // const categoryId = useSelector(state => state.filter.categoryId);
@@ -37,7 +45,7 @@ const Home = () => {
     dispatch(setCurrentPage(number))
   }
 
-  React.useEffect(() => {
+  const fetchPizzas = () => {
     setIsLoading(true);   
 
     const sortBy = sort.sortProperty.replace('-', '');
@@ -51,19 +59,55 @@ const Home = () => {
       setItems(res.data); // установили піци і ниже отключіли скєлєтон
       setIsLoading(false);
     });
+  }
+
+
+  // Если изменили параметры и был первый рендер, то делаем это действие
+  React.useEffect(() =>{
+    if (isMounted.current) {   // якщо був перший рендер
+      const queryString = qs.stringify({   //stringify превращає обьєкт в строчку
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      })
+      navigate(`?${queryString}`)
+      // console.log(queryString); // вернуло sortProperty=rating&categoryId=0&currentPage=1
+    }
+    isMounted.current = true;
+  }, [categoryId, sort.sortProperty, currentPage])
+
+
+  //Если был первый рендер, то проверяем URL-параметры и сохраняемся редуксе
+  React.useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      // console.log(params);
+      
+      const sort = list.find(obj => obj.sortProperty == params.sortProperty )
+
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        })
+      )
+      isSearch.current = true;
+    }
+  },[]) // при першому рендері тіпа
+
+  // Если был первый рендер, то запрашиваем пиццы 
+  React.useEffect(() => {
     window.scrollTo(0,0);
+
+    if(!isSearch.current) {
+      fetchPizzas();
+    }
+      isSearch.current = false;
       }, [categoryId, sort.sortProperty, searchValue, currentPage])
-  //   fetch(`https://63f1fde1aab7d09125ff6f7c.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
-  //   )
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((arr) => {
-  //       setItems(arr);
-  //       setIsLoading(false);
-  //     })
-  //     window.scrollTo(0,0); // оце шоб уверху сторінка з'являлася
-  // }, [categoryId, sort.sortProperty, searchValue, currentPage])
+
+
+
+
 
   const pizzas = items
   // .filter(obj => {  // це з фронта фільтр переробили на пошук в бекі, того це убрали
