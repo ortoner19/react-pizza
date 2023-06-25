912cdab71352a52a884442d9772ce9fd79570549
ortoner19 <ortoner@ukr.net>
2023-06-25 15:22:57 +0300

    lesson19

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React from "react";
+import React, { Children } from "react";
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import FullPizza from "./pages/FullPizza";
 import NotFound from "./pages/NotFound";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 
 // export const SearchContext = React.createContext();
 
+// function Parent({children}) {
+//   return <div>
+//     <h1>Заголовок</h1>
+//     {/* {children} */}
+//     <Outlet />
+//   </div>
+// }
+
 function App() {
   // const [searchValue, setSearchValue] = React.useState('');
-
   return (
-    <div className="wrapper">
-      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}> */}
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      {/* </SearchContext.Provider> */}
-    </div>
+    // <div className="wrapper">
+    // {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}> */}
+    // {/* <Header /> */}
+    // {/* <Parent>55555</Parent> */}
+    // <div className="content">
+    // <Routes>
+    // <Route path="/" element={<Home />} />
+    // <Route path="/cart" element={<Cart />} />
+    // <Route path="/pizza/:id" element={<FullPizza />} />
+    // <Route path="*" element={<NotFound />} />
+    // </Routes>
+    // </div>
+    // {/* </SearchContext.Provider> */}
+    // </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
